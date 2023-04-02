d576ab6e71ba169a3f1475cf24a1dfb75d43748b
João Magalhães <joamag@gmail.com>
2023-04-03 00:07:28 +0100

    fix: issue with dragging of files in Safari It was not working due to pedantic file validation.

--- react/components/overlay/overlay.tsx
+++ react/components/overlay/overlay.tsx
@@ -16,32 +16,58 @@ export const Overlay: FC<OverlayProps> = ({ text, style = [], onFile }) => {
     const classes = () =>
         ["overlay", visible ? "visible" : "", ...style].join(" ");
     useEffect(() => {
+        const isFile = (event: DragEvent) => {
+            // run a series of validations to determine if the event
+            // refers to a valid data transfer operation
+            if (!event.dataTransfer) return false;
+            if (!event.dataTransfer.items) return false;
+            if (!event.dataTransfer.types) return false;
+
+            // in case the items section of the data transfer
+            // is defined and no valid type is found, we assume
+            // that the user is dragging a binary file
+            if (
+                event.dataTransfer.items.length > 0 &&
+                !event.dataTransfer.items[0].type
+            ) {
+                return true;
+            }
+
+            // in case the types section of the data transfer
+            // is defined and the first type is "Files", we assume
+            // that the user is dragging a binary file
+            if (
+                event.dataTransfer.types.length > 0 &&
+                event.dataTransfer.types[0] === "Files"
+            ) {
+                return true;
+            }
+
+            return false;
+        };
         const onDrop = async (event: DragEvent) => {
-            if (!event.dataTransfer?.items) return;
-            if (event.dataTransfer?.items[0].type) return;
+            if (!isFile(event)) return;
+            if (!event.dataTransfer) return;
 
             setVisible(false);
 
-            const file = event.dataTransfer?.files[0];
+            const file = event.dataTransfer.files[0];
             onFile && onFile(file);
 
             event.preventDefault();
             event.stopPropagation();
         };
         const onDragOver = async (event: DragEvent) => {
-            if (!event.dataTransfer?.items) return;
-            if (event.dataTransfer?.items[0].type) return;
+            if (!isFile(event)) return;
             setVisible(true);
             event.preventDefault();
         };
         const onDragEnter = async (event: DragEvent) => {
-            if (!event.dataTransfer?.items) return;
-            if (event.dataTransfer?.items[0].type) return;
+            if (!isFile(event)) return;
             setVisible(true);
         };
         const onDragLeave = async (event: DragEvent) => {
-            if (!event.dataTransfer?.items) return;
-            if (event.dataTransfer?.items[0].type) return;
+            if (!isFile(event)) return;
             setVisible(false);
         };
         document.addEventListener("drop", onDrop);
