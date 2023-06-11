3c152b0b39467cd88344d49d2f231fca7114a026
João Magalhães <joamag@gmail.com>
2023-06-11 12:24:59 +0100

    chore: new text input sizes

--- react/components/text-input/text-input.tsx
+++ react/components/text-input/text-input.tsx
@@ -4,16 +4,18 @@ import "./text-input.css";
 
 type TextInputProps = {
     type?: "text" | "password" | "email" | "number" | "search" | "tel" | "url";
+    size?: "small" | "medium" | "large";
     placeholder?: string;
     style?: string[];
 };
 
 export const TextInput: FC<TextInputProps> = ({
     type = "text",
+    size = "large",
     placeholder,
     style = []
 }) => {
-    const classes = () => ["text-input", ...style].join(" ");
+    const classes = () => ["text-input", size, ...style].join(" ");
     return (
         <input className={classes()} type={type} placeholder={placeholder} />
     );
