9f3b68789eb590e9e8f9f131d5f4b2383d6c1e90
João Magalhães <joamag@gmail.com>
2023-09-22 01:10:56 +0100

    feat: improved text input support Support for the onchange event.

--- react/components/text-input/text-input.tsx
+++ react/components/text-input/text-input.tsx
@@ -5,19 +5,29 @@ import "./text-input.css";
 type TextInputProps = {
     type?: "text" | "password" | "email" | "number" | "search" | "tel" | "url";
     size?: "small" | "medium" | "large";
+    value?: string;
     placeholder?: string;
     style?: string[];
+    onChange?: (value: string) => void;
 };
 
 export const TextInput: FC<TextInputProps> = ({
     type = "text",
     size = "large",
+    value,
     placeholder,
-    style = []
+    style = [],
+    onChange
 }) => {
     const classes = () => ["text-input", size, ...style].join(" ");
     return (
-        <input className={classes()} type={type} placeholder={placeholder} />
+        <input
+            className={classes()}
+            type={type}
+            value={value}
+            placeholder={placeholder}
+            onChange={(e) => onChange && onChange(e.target.value)}
+        />
     );
 };
 
