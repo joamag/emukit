cd5f98dd4b727a9f988ee9ad9cbc1580ac662cd4
João Magalhães <joamag@gmail.com>
2024-08-06 19:18:51 +0100

    chore: better save info support

--- react/components/save-info/save-info.tsx
+++ react/components/save-info/save-info.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from "react";
 
+import Pair from "../pair/pair.tsx";
+import Info from "../info/info.tsx";
 import PanelTab from "../panel-tab/panel-tab.tsx";
 import { Emulator, rgbToDataUrl, SaveState } from "../../../ts/index.ts";
 
@@ -37,20 +39,42 @@ type SaveInfoTabProps = {
 };
 
 export const SaveInfoMain: FC<SaveInfoTabProps> = ({ saveState }) => (
-    <ul className="save-info-main">
-        <li>
-            <span>Model: </span>
-            <span>{saveState.model}</span>
-        </li>
-        <li>
-            <span>Agent: </span>
-            <span>{saveState.agent}</span>
-        </li>
-        <li>
-            <span>Size: </span>
-            <span>{saveState.size} bytes</span>
-        </li>
-    </ul>
+    <div className="save-info-main">
+        <Info>
+            <Pair key="model" name="Model" value={saveState.model} />
+            <Pair key="format" name="Format" value={saveState.format} />
+            <Pair key="agent" name="Agent" value={saveState.agent} />
+            <Pair
+                key="size"
+                name="Size"
+                value={
+                    saveState.size
+                        ? `${new Intl.NumberFormat().format(
+                              saveState.size
+                          )} bytes`
+                        : "-"
+                }
+            />
+            <Pair
+                key="date"
+                name="Date"
+                value={
+                    saveState.timestamp
+                        ? new Date(saveState.timestamp * 1000).toLocaleString(
+                              undefined,
+                              {
+                                  day: "2-digit",
+                                  month: "2-digit",
+                                  year: "numeric",
+                                  hour: "2-digit",
+                                  minute: "2-digit"
+                              }
+                          )
+                        : "-"
+                }
+            />
+        </Info>
+    </div>
 );
 
 export const SaveInfoThumbnail: FC<SaveInfoTabProps> = ({
@@ -70,3 +94,5 @@ export const SaveInfoThumbnail: FC<SaveInfoTabProps> = ({
         )}
     </div>
 );
+
+export default SaveInfo;
